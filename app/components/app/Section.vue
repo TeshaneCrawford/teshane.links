a058021a2851e87d06cd70cf857dad17b1cb3861
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-11-04 23:37:52 -0500

    update

--- app/components/app/Section.vue
+++ app/components/app/Section.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <section class="flex flex-col pb-8 pt-12 space-y-6 md:space-y-12">
+  <section class="flex flex-col pb-8 pt-12 min-h-dvh space-y-6 md:space-y-12">
     <AppBio />
     <AppLink />
   </section>
